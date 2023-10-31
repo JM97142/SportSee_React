78e5aaf515dba405d63631952fb560fe393b8e5b
JM97142 <jeanmichel.taret@gmail.com>
2023-10-31 12:59:45 +0100

    Création fichier et ajout mock

---
+++ src/components/userActivity/UserActivity.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+function UserActivity(props) {
+    // const [userData, setUserData] = useState([])
+
+    // useEffect(async (data) => {
+    //     const response = await fetch('http://localhost:3000/user/${userId}/activity')
+    //     const data = await response.json
+    //     setUserData(data)
+    // }, [])
+
+    // return (
+    //     <div>{userData.activity}</div>
+    // )
+}
+
+export default UserActivity
